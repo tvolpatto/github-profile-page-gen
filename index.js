212f79cb02ece702b363677fb7e051653b83915c
Thyago Volpatto <tvolpatto@gmail.com>
2020-01-26 16:37:49 -0700

    inquirer questions; package.json

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ var fs = require('fs');
 var axios = require('axios');
 
 const questions = [
-   
+    {
+      type: "input",
+      message: "What´s your Github username?",
+      name: "username" 
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "colors",
+        choices: [
+          "blue", 
+          "red", 
+          "pink", 
+          "green"
+        ]
+      }
 ];
 
 function writeToFile(fileName, data) {
@@ -11,7 +26,9 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-
+  inquirer.prompt(questions).then(function(answers) {
+      console.log(answers);
+  });
 }
 
 init();
