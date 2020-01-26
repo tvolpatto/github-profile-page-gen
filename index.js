9b7643efdc4bc9eebf86666aa78910e0a0361791
Thyago Volpatto <tvolpatto@gmail.com>
2020-01-26 16:19:28 -0700

    adding the pre built files from the homework

---
+++ index.js
@@ -0,0 +1,17 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+var axios = require('axios');
+
+const questions = [
+   
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+}
+
+init();
